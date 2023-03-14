b2b4a5c13afe81786e194e5c79d50d73953911ef
martinapiglio <martina.piglio@gmail.com>
2023-03-14 14:32:57 +0100

    vue script dynamic slider

--- js/script.js
+++ js/script.js
@@ -1,31 +1,3 @@
-/*
-const slides = [
-{
-    image: 'img/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morales',
-        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, {
-        image: 'img/02.webp',
-        title: 'Ratchet & Clank: Rift Apart',
-        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, {
-        image: 'img/03.webp',
-        title: 'Fortnite',
-        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    }, {
-        image: 'img/04.webp',
-        title: 'Stray',
-        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    }, {
-        image: 'img/05.webp',
-        title: "Marvel's Avengers",
-        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-    }
-];
-
-console.log(slides);
-*/
-
 const { createApp } = Vue
 
   createApp({
@@ -53,7 +25,32 @@ const { createApp } = Vue
                 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
                 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.'
             ]
-        }
+        },
+        activeImgIndex: 0
       }
+    },
+
+    methods: {
+
+        next() {
+
+            if (this.activeImgIndex < this.slides.images.length - 1) {
+                this.activeImgIndex++;
+            } else {
+                this.activeImgIndex = 0;
+            };
+
+        },
+
+        prev() {
+
+            if (this.activeImgIndex == 0) {
+                this.activeImgIndex = this.slides.images.length - 1;
+            } else {
+                this.activeImgIndex--;
+            };
+
+        }
+
     }
   }).mount('#app')
